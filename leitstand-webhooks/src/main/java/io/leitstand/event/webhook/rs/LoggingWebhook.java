43cccba36de93553fc5887066647c924c6045b11
Martin Steger <martin@rtbrick.com>
2020-02-20 02:02:43 +0100

    Assigned access token scopes to resources

--- leitstand-webhooks/src/main/java/io/leitstand/event/webhook/rs/LoggingWebhook.java
+++ leitstand-webhooks/src/main/java/io/leitstand/event/webhook/rs/LoggingWebhook.java
@@ -20,16 +20,19 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import java.util.logging.Logger;
 
-import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import io.leitstand.commons.rs.Resource;
 
 
-@RequestScoped
+@Resource
 @Path("webhooks")
 @Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
 public class LoggingWebhook {
 	
 	private static final Logger LOG = Logger.getLogger(LoggingWebhook.class.getName());
---
+++ leitstand-webhooks/src/main/java/io/leitstand/event/webhook/rs/Scopes.java
@@ -0,0 +1,10 @@
+package io.leitstand.event.webhook.rs;
+
+public final class Scopes {
+
+	public static final String ADM = "adm";
+	public static final String ADM_READ = "adm.read";
+	public static final String ADM_WEBHOOKS_READ = "adm.webhook.read";
+	public static final String ADM_WEBHOOKS = "adm.webhook";
+	
+}
